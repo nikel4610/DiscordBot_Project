2ab9c8afaff6169a447be5ff915c137f77b75f9e
Hyun-woo <nikel4610@gmail.com>
2024-01-29 15:43:36 +0900

    only crawling article

--- crawledNewsStory.py
+++ crawledNewsStory.py
@@ -8,15 +8,16 @@ from bs4 import BeautifulSoup
 url_prefix = 'https://www.yna.co.kr/news/'
 crawling_and_printing(url_prefix, 1, 2) # 범위 설정
 
-# //*[@id="articleWrap"]/div[2]/div/div/article = xpath
 # 위의 xpath 에 있는 <p></p> 태그 안에 있는 본문 내용을 크롤링 하는 def
 # url은 crawling_multiple()에서 받아온 headline_link
 
 def crawling_news_story(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    news_story = soup.select_one('#articleWrap > div.content01.scroll-article-zone01 > div > div > article')
-    print(news_story.text)
+    news_story = soup.select('#articleWrap > div.content01.scroll-article-zone01 > div > div > article > p')
+    news_story = [tag.text for tag in news_story]
+    news_story = ' '.join(news_story)
+    print(news_story)
     return news_story
 
 crawling_news_story('https://www.yna.co.kr/view/AKR20240129101900061?section=news')
